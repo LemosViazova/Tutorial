16c54d356f0188c7eb655df699d0029f54ef9e51
Miguel Lemos <miguellemos95@gmail.com>
2022-06-13 14:52:09 +0200

    Added the example we made today with Nastya

--- hello.ts
+++ hello.ts
@@ -1,1 +1,74 @@
 console.log("Hello World!")
+
+
+
+
+
+
+
+const sayHello = (name: String): String => 
+    "Hello, " + name
+
+console.log(
+    sayHello("Zara")
+)
+
+const family = [
+    "Miguelino",
+    "Scooby-Doo",
+       
+ "Nastya",
+ "Zara",
+ "Leonid",
+ "Enafa",
+ "Artem",
+ "Viktor",
+]
+
+family
+  .filter( // we filter only the familyMember that are NOT Miguelino
+      familyMember => familyMember != "Miguelino"
+  )
+  .map( // it takes every family person and says hello
+      familyMember => sayHello(familyMember)
+  )
+  .forEach( // it takes every hello and prints to console
+      name => console.log(name)
+)
+
+
+
+
+
+const say = (name: string,whatToSay:string): String =>
+name + "," + whatToSay
+console.log (
+    say("Bear","do you like honey?")
+)
+
+
+
+const animals = [
+    "elephant",
+    "fox",
+    "wolf",
+    "rabbit",
+    "bear",
+]
+
+const animalsWhoLikeHoney = [
+    "bear",
+    "fox",
+]
+animals.filter (
+    animal=> animalsWhoLikeHoney.includes(animal)
+)
+.forEach(
+    animal => console.log(animal)
+)
+
+const likesHoney = (animal: string): Boolean =>
+    animalsWhoLikeHoney.includes(animal)
+
+console.log("Bear likes honey?")
+console.log(likesHoney("bear"))
